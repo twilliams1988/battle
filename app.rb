ad0e7e294ee47362623204d305e46811ef9885ac
sus111 <sgraham535@gmail.com>
2016-07-18 15:08:13 +0100

    add cute cat

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+
+end
+
+get '/jason' do
+  "HI I'm Jason, but I like to be called MARLON"
+end
+
+get '/susan' do
+  "helloooo :) :D ;)"
+end
+
+get '/rainbow' do
+  "lemon drops and bunnies"
 end
