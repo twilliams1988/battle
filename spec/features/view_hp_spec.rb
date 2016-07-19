f1e5cd30f71fee14143d78f657dac0ce3fe05e4b
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-19 16:50:25 +0100

    completed task 21

--- spec/features/view_hp_spec.rb
+++ spec/features/view_hp_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'health points' do
   scenario 'view HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 100HP'
   end
 end
