be3f4ead3df01c66833f1010e36355f54fbe3627
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-19 15:09:44 +0100

    partially completed step 18

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,12 @@
+# require './app'
+require 'spec_helper'
+
+feature 'Enter player names' do #similar to rspec's describe
+  scenario "Users enter name" do #similar to rspec's it
+    visit('/')
+    fill_in :player_1_name, :with => "Player 1"
+    fill_in :player_2_name, :with => "Player 2"
+    click_button "Submit"
+    expect(page).to have_content("Player 1 vs Player 2!")
+  end
+end
