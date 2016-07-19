9e0341c88a278546dc41d123d9a374076bf93b41
Tommy Williams <tom@teow.co.uk>
2016-07-19 16:52:16 +0100

    Chapter 20 - Refactor RSpec

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,10 +4,7 @@
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Nik'
-    fill_in :player_2_name, with: 'Tommy'
-    click_button 'Submit'
+  sign_in_and_play
     expect(page).to have_content 'Nik vs. Tommy'
   end
 end
