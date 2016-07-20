fe9753d14fd164e5f49b427db0eefec722124314
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-20 11:34:42 +0100

    Creating player class walkthrough

--- app.rb
+++ app.rb
@@ -11,19 +11,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1_name = params[:player_1_name]
-    $player_2_name = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    Player.new($player_1_name)
-    Player.new($player_2_name)
-    @hit_message = session[:attack]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
