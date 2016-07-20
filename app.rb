24614066afa0e380cd71b883bcbf00a4d674d106
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-20 11:31:16 +0100

    Creating player class to store player names

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class Battle < Sinatra::Base
 
@@ -10,21 +11,19 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1_name = params[:player_1_name]
+    $player_2_name = params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    Player.new($player_1_name)
+    Player.new($player_2_name)
     @hit_message = session[:attack]
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
     erb :attack
   end
 
